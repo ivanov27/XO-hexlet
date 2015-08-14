194ee461d4f3f6d2767c45009f79ebb1295883dc
admin <cphglue@ukr.net>
2015-08-14 16:21:54 +0300

    FieldTest checks for the exeptions heve been added

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -1,5 +1,6 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exeptions.InvalidPointExeption;
 import org.junit.Test;
 
 import java.awt.*;
@@ -27,5 +28,55 @@ public class FieldTest {
 
         assertEquals(inputFigure,actualFigure);
 
+    }
+
+    @Test
+    public void testGetFigureWhenFigureIsNotSet() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, 0);
+        final  Figure actualFigure = field.getFigure(inputPoint);
+
+        assertNull(actualFigure);
+    }
+    @Test
+    public void testGetFigureWhenXIsLessThenZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1, 0);
+
+        try{
+            field.getFigure(inputPoint);
+            fail();
+        }catch (final InvalidPointExeption e){}
+    }
+    @Test
+    public void testGetFigureWhenYIsLessThenZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, -1);
+
+        try{
+            field.getFigure(inputPoint);
+            fail();
+        }catch (final InvalidPointExeption e){}
+    }
+
+    @Test
+    public void testGetFigureWhenXIsMoreThenSize() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(field.getSize() + 1, 0);
+
+        try{
+            field.getFigure(inputPoint);
+            fail();
+        }catch (final InvalidPointExeption e){}
+    }
+    @Test
+    public void testGetFigureWhenYIsMoreThenSize() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, field.getSize() + 1);
+
+        try{
+            field.getFigure(inputPoint);
+            fail();
+        }catch (final InvalidPointExeption e){}
     }
 }
