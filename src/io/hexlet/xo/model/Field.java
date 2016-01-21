80fca85b7a6a9987dea606636b2c9a9012c9f5bb
ivanov27vonavi <cphgroup@ukr.net>
2016-01-21 13:43:26 +0200

    Field - refactoring (rules part have bin removed)

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -1,7 +1,6 @@
 package io.hexlet.xo.model;
 
 
-import io.hexlet.xo.model.exeptions.AlreadyOccupiedException;
 import io.hexlet.xo.model.exeptions.InvalidPointExeption;
 
 import java.awt.*;
@@ -14,7 +13,9 @@ public class Field {
     public static final int FIELD_SIZE = 3;
     public static final int MIN_COORDINATE = 0;
     public static final int MAX_COORDINATE = FIELD_SIZE;
+
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+
     public int getSize(){
         return FIELD_SIZE;
     }
@@ -26,12 +27,9 @@ public class Field {
         return field[point.x] [point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure)throws InvalidPointExeption, AlreadyOccupiedException{
+    public void setFigure(final Point point, final Figure figure)throws InvalidPointExeption{
         if(!checkPoint(point)){
             throw new InvalidPointExeption();
-        }
-        if(field[point.x] [point.y] != null){
-            throw  new AlreadyOccupiedException();
         }
         field[point.x] [point.y] = figure;
 
